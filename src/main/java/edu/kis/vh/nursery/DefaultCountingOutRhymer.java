5191dc93551bd9bdd2e1c14e5e50c1f4700d7707
Kamil Kaźmierczak <kamilkazmierczak@MacBook-Air-Kamil.local>
2024-05-14 17:32:31 +0200

    9. Zastosowanie możliwie najmniejszej widoczności dla atrybutów klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int NUMBERS_SIZE = 12;
-    public static final int MINUS_ONE = -1;
-    public static final int NUMBERS_CAPACITY = 11;
+    private static final int NUMBERS_SIZE = 12;
+    private static final int MINUS_ONE = -1;
+    private static final int NUMBERS_CAPACITY = 11;
     private final int[] numbers = new int[NUMBERS_SIZE];
 
-    public int total = MINUS_ONE;
+    private int total = MINUS_ONE;
 
     public void countIn(int in) {
         if (!isFull())
