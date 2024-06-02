f2e7b409702b24d37bd95668fe9f193857f2c46d
Kamil Kaźmierczak <kamilkazmierczak@MacBook-Air-Kamil.local>
2024-06-02 21:13:46 +0200

    17. Opracowanie dokumentacji do metod klasy IntLinkedList.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -36,19 +36,39 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     * Metoda isEmpty() sprawdza czy lista jest pusta.
+     *
+     * @return true, jeżeli lista jest pusta, w przeciwnym wypadku false.
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Metoda isFull() sprawdza czy lista jest pełna.
+     *
+     * @return false, ponieważ lista nie ma określonego limitu elementów wewnątrz niej.
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Metoda top() zwraca wartość z ostatniego węzła na liście.
+     *
+     * @return wartość z ostatniego węzła, jeżeli lista nie jest pusta, w przeciwnym wypadku -1.
+     */
     public int top() {
         if (isEmpty()) return -1;
         return last.getValue();
     }
 
+    /**
+     * Metoda pop() usuwa i zwraca wartość z ostatniego węzła na liście.
+     *
+     * @return wartość z ostatniego węzła, jeżeli lista nie jest pusta, w przeciwnym wypadku -1.
+     */
     public int pop() {
         if (isEmpty()) return -1;
         int ret = last.getValue();
@@ -57,6 +77,12 @@ public class IntLinkedList {
     }
 
     //    Dodatkowa metoda do dodawania elementów
+
+    /**
+     * Metoda push(value) dodaje nowy węzeł z podaną wartością na koniec listy.
+     *
+     * @param value wartość, która ma zostać nadana w nowo-powstałym elemencie listy.
+     */
     public void push(int value) {
         if (isEmpty()) last = new Node(value);
         else last.setPrev(new Node(value));
